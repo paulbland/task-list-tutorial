be1720833b4670e889fdfe19a55c18b07ed95bd5
Paul Bland <paul.bland@gmail.com>
2024-05-08 21:41:12 -0400

    first commit

---
+++ ui/src/components/Task.js
@@ -0,0 +1,61 @@
+import { Button, Checkbox, Typography } from "@mui/material";
+import React, { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { UpdateTaskForm } from "./UpdateTaskForm";
+import classNames from "classnames";
+import axios from "axios";
+import { API_URL } from "../utils";
+
+export const Task = ({ task, fetchTasks }) => {
+  const { id, name, completed } = task;
+  const [isComplete, setIsComplete] = useState(completed);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const handleUpdateTaskCompletion = async () => {
+    try {
+      await axios.put(API_URL, {
+        id, name, complete: !isComplete
+      })
+      setIsComplete((prev) => !prev);
+    } catch (err) {
+      console.log(err)
+    }
+    
+  };
+  const handleDeleteTask = async (id) => {
+    try {
+      await axion.delete(`${API_URL}/${task.id}`);
+      await fetchTasks();
+    } catch (err) {
+      console.log(err)
+    }
+    console.log("Handle delete task");
+
+  };
+  return (
+    <div>
+      <div
+        className={classNames("flex", {
+          done: isComplete
+        })}
+      >
+        <Checkbox checked={isComplete} onChange={handleUpdateTaskCompletion} />
+        <Typography valiant="h4">{name}</Typography>
+      </div>
+      <div className="taskButtons">
+        <Button variant="contained" onClick={() => setIsDialogOpen(true)}>
+          <EditIcon />
+        </Button>
+        <Button color="error" variant="contained" onClick={handleDeleteTask}>
+          <DeleteIcon />
+        </Button>
+      </div>
+      <UpdateTaskForm
+        fetchTasks={fetchTasks}
+        isDialogOpen={isDialogOpen}
+        setIsDialogOpen={setIsDialogOpen}
+        task={task}
+      />
+    </div>
+  );
+};
